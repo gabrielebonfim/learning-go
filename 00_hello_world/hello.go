d6b22163dba1ac802c2a63e12f7e75c3f10512a8
Gabriele Alves <gabriele.bonfim@luizalabs.com>
2023-11-21 15:36:57 -0500

    hello world

---
+++ 00_hello_world/hello.go
@@ -0,0 +1,7 @@
+package main // The main package is the start point of a Go program execution
+
+import "fmt" // Standard package that implements formattedd I/O.
+
+func main() { // Start point function
+	fmt.Println ("Hello world!") // The Println has 2 functions: "Print" is a fmt method that prints the message in console (output). "ln" is responsible to breakline after execution.
+}
